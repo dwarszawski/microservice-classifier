6ae5ac93c7c1160e14ec474eb6adc93549a46dac
Adam Sznajder <asznajder@sumologic.com>
2016-02-04 20:58:43 +0100

    Added handling Meetup events

--- src/main/scala/scalawaw/app/ClassifierService.scala
+++ src/main/scala/scalawaw/app/ClassifierService.scala
@@ -5,7 +5,7 @@ import akka.util.Timeout
 import spray.json.DefaultJsonProtocol
 
 import scala.concurrent.duration._
-import scalawaw.app.HandledJsons.FBEvent
+import scalawaw.app.HandledJsons.{MeetupEvent, FBEvent}
 import scalawaw.service.com.detector.console.http.service.HttpService
 
 trait JsonProtocol extends DefaultJsonProtocol {
@@ -15,11 +15,14 @@ trait JsonProtocol extends DefaultJsonProtocol {
   implicit val fbPlaceFormat = jsonFormat3(FBPlace)
   implicit val fbEventFormat = jsonFormat5(FBEvent)
 
-  implicit val requestFormat = jsonFormat3(ClassificationRequest)
+  implicit val meetupGroupFormat = jsonFormat8(MeetupGroup)
+  implicit val meetupResultsFormat = jsonFormat17(MeetupEvent)
+
+  implicit val requestFormat = jsonFormat4(ClassificationRequest)
 }
 
 case class ClassificationRequest(fbProfile: String, meetupProfile: String,
-                                 fbEvents: Seq[FBEvent])
+                                 fbEvents: Seq[FBEvent], meetupEvents: Seq[MeetupEvent])
 
 trait ClassifierService extends HttpService with JsonProtocol with SprayJsonSupport {
 
